dce402dd76a899c9785953127319b25ce887a2ae
stmaho <121847812+stmaho@users.noreply.github.com>
2023-03-14 00:33:44 +0900

    編集ページの遷移をリダイレクト

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -54,7 +54,7 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? == current_user
+    unless user_signed_in? == current_user.name
       redirect_to action: :index
     end
   end
