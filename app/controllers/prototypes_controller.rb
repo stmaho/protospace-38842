ea7a2490a85fef30595adc7a660723c0a7ee740b
stmaho <121847812+stmaho@users.noreply.github.com>
2023-03-14 00:45:25 +0900

    編集ページの遷移をリダイレクト

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy, :update]
   before_action :move_to_index, except: [:index, :show,]
   def index
     #@name = current_user.name
