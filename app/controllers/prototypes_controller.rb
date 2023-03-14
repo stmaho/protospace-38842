5b59e9f3fc86f8536a6e5e5c2bf85c7c64e169d5
stmaho <121847812+stmaho@users.noreply.github.com>
2023-03-14 16:26:47 +0900

    編集ページの遷移をリダイレクト

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :move_to_index, except: [:index, :show,]
+  before_action :authenticate_user!, expect: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
   def index
     #@name = current_user.name
     @prototype = Prototype.all
@@ -55,7 +55,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == prototype.user_id
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
      redirect_to action: :index
     end
   end
