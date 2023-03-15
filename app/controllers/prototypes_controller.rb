8c67186618d56f0e272ede42862a36250d9b7ddc
stmaho <121847812+stmaho@users.noreply.github.com>
2023-03-15 18:13:33 +0900

    新規投稿画面への遷移

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, expect: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
   def index
     #@name = current_user.name
